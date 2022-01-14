83122fd35e41e32463b0928860e0a76ab11eea9c
noriakis <noriaki.sato53@gmail.com>
2022-01-14 18:40:56 +0900

    update main function

--- R/wcGeneSummary.R
+++ R/wcGeneSummary.R
@@ -13,6 +13,7 @@
 #' @param corThresh the correlation threshold
 #' @param layout the layout for correlation network, defaul to "nicely"
 #' @param edgeLink if FALSE, use geom_edge_diagonal
+#' @param deleteZeroDeg delete zero degree node from plot in correlation network
 #' @return list of data frame and ggplot2 object
 #' @import tm
 #' @import GeneSummary
@@ -29,15 +30,19 @@
 #' 
 wcGeneSummary <- function (geneList, excludeFreq=5000, additionalRemove=NA, madeUpper=c("dna","rna"), organism=9606,
                            palette=c("blue","red"), numWords=15, scaleRange=c(5,10),
-                           plotType="wc", corThresh=0.6, layout="nicely", edgeLink=TRUE, ...) {
+                           plotType="wc", corThresh=0.6, layout="nicely", edgeLink=TRUE, deleteZeroDeg=TRUE, ...) {
     returnList <- list()
+    ## Load from GeneSummary
     tb <- loadGeneSummary(organism = organism)
 
-    # load("allFreqGeneSummary.rda") ## Already performed
+    # load("allFreqGeneSummary.rda") # Already performed, and automatically loaded
+
+    ## Filter high frequency words
     filterWords <- allFreqGeneSummary[allFreqGeneSummary$freq>excludeFreq,]$word
-    filterWords <- c(filterWords, "pmids")
+    filterWords <- c(filterWords, "pmids") # 'PMIDs' is excluded by default
     fil <- tb %>% filter(Gene_ID %in% geneList)
     
+    ## Make corpus
     docs <- VCorpus(VectorSource(fil$Gene_summary))
     docs <- docs %>%
         tm_map(FUN=content_transformer(tolower)) %>% 
@@ -50,24 +55,33 @@ wcGeneSummary <- function (geneList, excludeFreq=5000, additionalRemove=NA, made
         docs <- docs %>% tm_map(removeWords, additionalRemove)
     }
     
+    ## Set parameters for correlation network
     if (is.na(corThresh)){corThresh<-0.6}
     if (is.na(numWords)){numWords<-10}
     docs <- TermDocumentMatrix(docs)
     
     if (plotType=="network"){
-        mft <- findMostFreqTerms(docs, n = numWords)
-        freq <- mft[[1]]
-        freqWords <- names(mft[[1]])
+        mat <- as.matrix(docs)
+        matSorted <- sort(rowSums(mat), decreasing=TRUE)
+        matSorted <- matSorted[1:numWords]
+        freqWords <- names(matSorted)
         freqWordsDTM <- t(as.matrix(docs[Terms(docs) %in% freqWords, ]))
+        
+        ## Check correlation
         corData <- cor(freqWordsDTM)
+
+        ## Set correlation below threshold to zero
         corData[corData<corThresh] <- 0
         coGraph <- graph.adjacency(corData, weighted=TRUE, diag = FALSE)
-        V(coGraph)$Freq <- freq[V(coGraph)$name]
+        V(coGraph)$Freq <- matSorted[V(coGraph)$name]
         nodeName <- V(coGraph)$name
         for (i in madeUpper) {
             nodeName[nodeName == i] <- toupper(i)
         }
         V(coGraph)$name <- nodeName
+        if (deleteZeroDeg){
+            coGraph <- induced.subgraph(coGraph, degree(coGraph) > 0)
+        }
         netPlot <- ggraph(coGraph, layout=layout)
         if (edgeLink){
             netPlot <- netPlot + geom_edge_link(aes(width=weight, color=weight), alpha=0.5, show.legend = F)
