f9b7844799284c70b317586c93688b1e70ea763a
noriakis <noriaki.sato53@gmail.com>
2022-05-11 13:48:06 +0900

    added a function: textORA

---
+++ R/textORA.R
@@ -0,0 +1,48 @@
+#' textORA
+#' 
+#' Return p-values from hypergeometric distribution
+#' 
+#' @param queries gene list (Entrez ID)
+#' @return p-values for the words
+#' @import GeneSummary tm
+#' @export
+#' 
+textORA <- function(queries) {
+    tb <- loadGeneSummary()
+    
+    fil <- tb |> dplyr::filter(Gene_ID %in% geneList)
+    
+    ## Make corpus for queried genes
+    docs <- VCorpus(VectorSource(fil$Gene_summary))
+    docs <- docs |>
+        tm_map(FUN=content_transformer(tolower)) |> 
+        tm_map(FUN=removeNumbers) |>
+        tm_map(removeWords, stopwords::stopwords("english", "stopwords-iso")) |>
+        tm_map(FUN=removePunctuation) |>
+        tm_map(FUN=stripWhitespace)
+    
+    docs <- TermDocumentMatrix(docs)
+    mat <- as.matrix(docs)
+    matSorted <- sort(rowSums(mat), decreasing=TRUE)
+    
+    pvs <- sapply(names(matSorted), function (x) returnP(x, matSorted, allFreqGeneSummary))
+    pvs
+}
+
+#' returnP
+#' 
+#' Calculate p-values
+#' @param name name of a gene
+#' @param matSorted words frequency in the cluster
+#' @param allFreqGeneSummary words frequency in the whole description
+#' @return p-values for the words
+#' 
+returnP <- function(name, matSorted, allFreqGeneSummary){
+    query <- as.numeric(matSorted[name])
+    noquery <- sum(matSorted) - query
+    queryAll <- as.numeric(allFreqGeneSummary[name, "freq"])
+    allwords <- sum(allFreqGeneSummary$freq) - queryAll
+    
+    ## p-value
+    return(sum(dhyper(query:sum(matSorted), queryAll, allwords, sum(matSorted))))
+}
