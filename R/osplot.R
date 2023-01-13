ce026621cba65b617de206c5188d47702ca259ab
noriakis <31095487+noriakis@users.noreply.github.com>
2023-01-13 22:57:16 +0900

    update functions

--- R/osplot.R
+++ R/osplot.R
@@ -2,7 +2,7 @@
 #' 
 #' wrapper for functions wcGeneSummary, wcAbst, and wcBSDB
 #' 
-#' @param target "abstract", "bugsigdb", "refseq"
+#' @param target "pubmed", "bugsigdb", "refseq", "ec"
 #' @param ... passed to each function
 #' @return list of data frames and ggplot2 object
 #' 
@@ -12,13 +12,15 @@
 #' @export
 #' 
 osplot <- function(target, ...) {
-	if (target=="abstract"){
+	if (target=="pubmed"){
 		return(wcAbst(...))
 	} else if (target=="bugsigdb"){
 		return(wcBSDB(...))
 	} else if (target=="refseq"){
 		return(wcGeneSummary(...))
+	} else if (target=="ec") {
+		return(wcEC(...))
 	} else {
-		stop("Please specify abstract, bugsigdbr, or refseq")
+		stop("Please specify pubmed, bugsigdbr, or refseq")
 	}
 }
---
+++ R/wcEC.R
@@ -0,0 +1,56 @@
+#' wcEC
+#' 
+#' Query the Enzyme Comission number and obtain description,
+#' and search pubmed for these enzymes and make word cloud and
+#' correlation network. Need "enzyme.dat" from ExPASy
+#' (https://enzyme.expasy.org/).
+#' 
+#' @param file file downloaded from expasy
+#' @param ecnum candidate ecnum, like those obtained from eggNOG-mapper
+#' @param ... passed to osplot(target="pubmed")
+#' @export
+#' 
+
+wcEC <- function(file, ecnum, ...) {
+  flg <- FALSE
+  candecs <- NULL
+  con = file(file, "r")
+  while ( TRUE ) {
+    line = readLines(con, n = 1)
+    if ( length(line) == 0 ) {
+      break
+    }
+    if (startsWith(line,"ID")) {
+      ccs <- NULL
+      ecs <- NULL
+      ec <- gsub("ID   ","",line)
+      if (ec %in% ecnum) {
+        flg <- TRUE
+      } else {
+        flg <- FALSE
+      }
+    }
+    if (flg) {
+      if (startsWith(line, "DE")) {
+        de <- gsub("\\.","",gsub("DE   ","",line))
+      }
+      if (startsWith(line, "CC")) {
+        cc <- gsub("\\.","",gsub("CC   ","",line))
+        cc <- gsub("-!- ", "", cc)
+        ccs <- c(ccs, cc)
+      }
+      if (startsWith(line, "//")) {
+        flg <- FALSE
+        ecs <- c(ec, de, paste0(ccs, collapse=" "))
+        candecs <- rbind(candecs, ecs)
+        }
+    }
+  }
+  close(con)
+  candecs <- data.frame(candecs) |>
+    `colnames<-`(c("number","desc","comment"))
+  quoted <- dQuote(candecs$desc)
+  abst <- osplot(target="pubmed",
+                 quoted, ...)
+  return(abst)
+}
