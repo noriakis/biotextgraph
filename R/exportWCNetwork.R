dc02256fc77cb4dd0c043bc4fb317e1d381d5da1
noriakis <nori@hgc.jp>
2023-07-21 15:07:37 +0900

    added exportWCNetwork.R

---
+++ R/exportWCNetwork.R
@@ -0,0 +1,65 @@
+#' exportWCNetwork
+#' 
+#' export the wordcloud network provided the gene cluster network
+#' Node size will be the number of genes in the cluster
+#' 
+#' @param g igraph (node name corresponding to gene cluster name)
+#' @param geneList named list of gene IDs
+#' @param dir directory to output the files, default to current directory
+#' @param keyType type of gene IDs (default to SYMBOL)
+#' @param colors color palettes to use for each cluster
+#' named list, or random palette will be assigned if NULL
+#' @param orgDb annotation database
+#' @param wcArgs parameters for ggwordcloud
+#' @param numWords number of words
+#' @param scaleMin minimum scale size for nodes
+#' @param scaleMax maximum scale size for nodes
+#' @param wcScale scaling size for wordcloud
+#' @return export the Cytoscape.js network
+exportWCNetwork <- function(g, geneList, dir="network", colors=NULL,
+	scaleMin=50, scaleMax=200, wcScale=20,
+	keyType="SYMBOL", orgDb=org.Hs.eg.db, wcArgs=list(), numWords=50) {
+
+	## Check colors
+	if (is.null(colors)) {
+		colors <- list()
+		for (gl in names(geneList)) {
+			colors[[gl]] <- brewer.pal(10, sample(row.names(RColorBrewer::brewer.pal.info), 1))
+		}
+	}
+
+	## Set size
+	sizes <- lapply(geneList, function(x) length(x)) |> unlist()
+	V(g)$size <- as.numeric(sizes[V(g)$name])
+
+	## Export images
+	dir.create(paste0(dir))
+	dir.create(paste0(dir, "/images"))
+
+	images <- NULL
+	for (gl in names(geneList)) {
+		wc <- refseq(geneList[[gl]],
+			keyType=keyType,
+			orgDb=orgDb,
+			plotType="wc",
+			wcScale=wcScale,
+			numWords=numWords)@wc
+	    ggsave(paste0(dir, "/images/", gl ,".png"),
+            wc, dpi=300, width=10, height=10)
+	    images <- c(images, paste0("images/", gl ,".png"))
+	}
+
+	V(g)$image <- images
+    V(g)$shape <- rep("circle", length(V(g)))
+
+	## Scale the node size
+	sizeMin <- 50
+	sizeMax <- 200
+	rawMin <- min(V(g)$size)
+	rawMax <- max(V(g)$size)
+	scf <- (sizeMax-sizeMin)/(rawMax-rawMin)
+
+	V(g)$size <- scf * V(g)$size + sizeMin - scf * rawMin
+	exportCyjs(g, ".", dir)
+
+}
